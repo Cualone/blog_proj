fb5ffc98a1456382dbd8a8eb85ec31a140da26d5
Cualone <june568@naver.com>
2023-04-16 01:34:57 +0900

    ã초기설정

---
+++ src/main/java/com/example/blogmake/BlogmakeApplication.java
@@ -0,0 +1,13 @@
+package com.example.blogmake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogmakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogmakeApplication.class, args);
+	}
+
+}
